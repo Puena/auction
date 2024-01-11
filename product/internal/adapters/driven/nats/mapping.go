3ea6cacabcd3094fe2f96f8168656cc3a07d835a
Dmitry P <woweuropeluffy@gmail.com>
2024-01-12 00:53:05 +0300

    add main

--- product/internal/adapters/driven/nats/mapping.go
+++ product/internal/adapters/driven/nats/mapping.go
@@ -1,9 +1,11 @@
 package nats
 
 import (
+	"github.com/Puena/auction/pbgo/auction"
 	"github.com/Puena/auction/product/internal/core/domain"
 )
 
 func eventProductToProtoProduct(eventProduct domain.Product) int {
+	auction.Product{}
 	return 0
 }
